dbd448001a7cbfec67c3d2e9d26d6aa60a7a525f
ramdanii19 <muhamadramdani0519@gmail.com>
2026-06-16 03:05:07 +0700

    feat: add cron scheduler for late order handling

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 import { env } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import app from "./app.js";
+import { startLateOrderCron } from "./jobs/lateOrder.cron.js";
 
 await connectDB();
+
+// Toggle via ENABLE_CRON=true di .env. Off by default agar tidak jalan saat test/dev tanpa sengaja.
+if (process.env.ENABLE_CRON === "true") {
+  startLateOrderCron();
+}
 
 app.listen(env.port, () => {
   console.log(`Server running on port ${env.port} [${env.nodeEnv}]`);
