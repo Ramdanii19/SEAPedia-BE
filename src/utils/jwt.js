e564fd57c9cc8b5a6b0a55c71ce1c7febd9a0213
ramdanii19 <muhamadramdani0519@gmail.com>
2026-06-16 03:17:50 +0700

    feat: harden session and token expiry handling

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,8 +1,9 @@
 import jwt from "jsonwebtoken";
+import { randomUUID } from "crypto";
 import { env } from "../config/env.js";
 
 export function signToken(payload) {
-  return jwt.sign(payload, env.jwtSecret, { expiresIn: env.jwtExpiresIn });
+  return jwt.sign({ ...payload, jti: randomUUID() }, env.jwtSecret, { expiresIn: env.jwtExpiresIn });
 }
 
 export function verifyToken(token) {
